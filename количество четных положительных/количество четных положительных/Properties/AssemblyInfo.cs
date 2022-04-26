c3328855f38141a175f46386a4dd207da4a1fc68
YaraMm <for.steam2016@mail.ru>
2022-04-26 21:59:22 +0400

    добавил сортировку пузырьком

---
+++ количество четных положительных/количество четных положительных/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("количество четных положительных")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("количество четных положительных")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("df2e26c0-e5f7-4d7e-b1be-b1cfa033e546")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ сортировка пузырьком/сортировка пузырьком/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace сортировка_пузырьком
+{
+    class Program
+    {
+        public static string BubbleSort(List<int> myList)
+        {
+            for (int i = 0; i < myList.Count; i++)
+            {
+                for (int j = 0; j < myList.Count - 1 - i; j++)
+                {
+                    if (myList[j] > myList[j + 1])
+                    {
+                        var variety = myList[j];
+                        myList[j] = myList[j + 1];
+                        myList[j + 1] = variety;
+                    }
+                }
+            }
+            string res = "";
+            for (int i = 0; i < myList.Count; i++) res += myList[i];
+            return res;
+        }
+        static void Main(string[] args)
+        {
+            var myList = new List<int> { 9, 5, 7, 1, 6, 3, 2, 4, 8, 0 };
+            Console.WriteLine(BubbleSort(myList));
+        }
+    }
+}
